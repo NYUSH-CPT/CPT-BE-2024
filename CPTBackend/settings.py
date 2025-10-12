a2df744b27bc5a05b90c45f897d8c9525c66262c
YongyuLiu03 <yl8126@nyu.edu>
2025-10-12 16:08:16 -0500

    static file

--- CPTBackend/settings.py
+++ CPTBackend/settings.py
@@ -153,7 +153,6 @@ TIME_ZONE = 'Asia/Hong_Kong'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
