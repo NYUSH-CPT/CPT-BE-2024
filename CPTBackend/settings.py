d6996cc8400c4b650a6591adf5403ffa40bd8381
yongyu_liu <yl8126@nyu.edu>
2025-12-04 12:28:02 -0600

    Update settings.py

--- CPTBackend/settings.py
+++ CPTBackend/settings.py
@@ -114,12 +114,7 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', 3306),
-        'CONN_MAX_AGE': 600,
-        'OPTIONS': {
-            'ssl': {
-                'disabled': True
-            }
-        }
+        'CONN_MAX_AGE': 600
     }
 }
 
